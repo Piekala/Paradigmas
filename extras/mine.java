6a2e469fa41a500c0417b69ac3dcf2f263e6e675
Piekala <37380454+Piekala@users.noreply.github.com>
2018-05-16 16:05:29 -0300

    pensamento inicial

--- extras/mine.java
+++ extras/mine.java
@@ -1,1 +1,38 @@
+class coordenadas {
+  protected int x;
+  protected int y;
+  protected int z;
+  public coordenadas(int x, int y, int z) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+  }
+}
 
+abstract class bloco {
+  protected String nome;
+  protected coordenadas cord;
+  protected int tam;
+  public abstract void quebra();
+  public abstract void dropa();
+}
+
+class madeira extends bloco {
+
+}
+
+class terra extends bloco {
+
+}
+
+class grama extends bloco{
+
+}
+
+class pedregulho extends bloco {
+
+}
+
+class pedra extends bloco {
+
+}
